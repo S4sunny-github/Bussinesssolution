096a8b32fb0fa3de0661dde606be022f74ccc8ae
Sunny.K.bhatt <62854297+S4sunny-github@users.noreply.github.com>
2023-10-11 17:13:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Example JavaScript code in script.js
+
+function greetUser() {
+    alert('Welcome to our business solution website!');
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    // Execute code after the DOM has fully loaded
+    greetUser();
+  });
+  
